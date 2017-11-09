772337b583ab8344f242598ee940ba699490c859
Lucio Rossi <lucio.rossi75@gmail.com>
2017-11-09 21:24:11 +0100

    feat: experimental bendy_face

--- rigs/experimental/bendy_face.py
+++ rigs/experimental/bendy_face.py
@@ -11,6 +11,7 @@ from   ..widgets import create_face_widget, create_eye_widget, create_eyes_widge
 class Rig:
 
     CTRL_SCALE = 0.1
+    MCH_SCALE = 0.3
 
     def __init__(self, obj, bone_name, params):
         self.obj = obj
@@ -26,19 +27,45 @@ class Rig:
                 for child in edit_bone.children_recursive:
                     self.bones['org'].remove(child.name)
 
+        self.start_bones = self.get_start_bones()
         self.bones['ctrl'] = dict()
+        self.bones['mch'] = dict()
+        self.bones['def'] = dict()
+
+    def create_mch(self):
+        bpy.ops.object.mode_set(mode='EDIT')
+        edit_bones = self.obj.data.edit_bones
+
+        for name in self.start_bones:
+            subchain = [name]
+            subchain.extend(connected_children_names(self.obj, name))
+            self.bones['mch'][strip_org(name)] = []
+
+            for subname in subchain:
+                mch = copy_bone(self.obj, subname, assign_name=make_mechanism_name(strip_org(subname)))
+                edit_bones[mch].parent = None
+                edit_bones[mch].length *= self.MCH_SCALE
+                self.bones['mch'][strip_org(name)].append(mch)
+
+    def create_def(self):
+        bpy.ops.object.mode_set(mode='EDIT')
+        edit_bones = self.obj.data.edit_bones
+
+        for name in self.start_bones:
+            subchain = [name]
+            subchain.extend(connected_children_names(self.obj, name))
+            self.bones['def'][strip_org(name)] = []
+
+            for subname in subchain:
+                def_bone = copy_bone(self.obj, subname, assign_name=make_deformer_name(strip_org(subname)))
+                edit_bones[def_bone].parent = None
+                self.bones['def'][strip_org(name)].append(def_bone)
 
     def create_controls(self):
         bpy.ops.object.mode_set(mode='EDIT')
         edit_bones = self.obj.data.edit_bones
 
-        start_bones = []
-
-        for name in self.bones['org'][1:]:
-            if not edit_bones[name].use_connect:
-                start_bones.append(name)
-
-        for name in start_bones:
+        for name in self.start_bones:
             subchain = [name]
             subchain.extend(connected_children_names(self.obj, name))
             self.bones['ctrl'][strip_org(name)] = []
@@ -55,13 +82,110 @@ class Rig:
             edit_bones[last_ctrl].length *= self.CTRL_SCALE
             self.bones['ctrl'][strip_org(name)].append(last_ctrl)
 
+        self.aggregate_ctrls()
+
         bpy.ops.object.mode_set(mode='OBJECT')
         for subchain in self.bones['ctrl']:
             for ctrl in self.bones['ctrl'][subchain]:
                 create_sphere_widget(self.obj, ctrl)
 
+    def aggregate_ctrls(self):
+        bpy.ops.object.mode_set(mode='EDIT')
+        edit_bones = self.obj.data.edit_bones
+
+        aggregates = []
+
+        all_ctrls = []
+
+        for subchain in self.start_bones:
+            for ctrl in self.bones['ctrl'][strip_org(subchain)]:
+                all_ctrls.append(ctrl)
+
+        while 1:
+            ctrl = all_ctrls[0]
+            aggregate = [ctrl]
+            for ctrl2 in all_ctrls[1:]:
+                if edit_bones[ctrl].head == edit_bones[ctrl2].head:
+                    aggregate.append(ctrl2)
+            for element in aggregate:
+                all_ctrls.remove(element)
+            if len(aggregate) > 1:
+                aggregates.append(aggregate)
+            if not all_ctrls:
+                break
+
+        if aggregates:
+            self.bones['ctrl']['aggregate'] = []
+
+        for aggregate in aggregates:
+            name = self.get_aggregate_name(aggregate)
+            aggregate_ctrl = copy_bone(self.obj, aggregate[0], name)
+            self.bones['ctrl']['aggregate'].append(aggregate_ctrl)
+            for ctrl in aggregate:
+                edit_bones.remove(edit_bones[ctrl])
+                for subchain in self.start_bones:
+                    if ctrl in self.bones['ctrl'][strip_org(subchain)]:
+                        self.bones['ctrl'][strip_org(subchain)].remove(ctrl)
+                        continue
+
+        return True
+
+    def get_aggregate_name(self, aggregate):
+
+        total = '.'.join(aggregate)
+
+        root = aggregate[0].split('.')[0]
+        for name in aggregate[1:]:
+            if name.split('.')[0] not in root:
+                root = '.'.join([root, name.split('.')[0]])
+
+        name = root
+
+        t_b = ''
+        if 'T' in total and 'B' in total:
+            t_b = ''
+        elif 'T' in total:
+            t_b = 'T'
+        elif 'B' in total:
+            t_b = 'B'
+
+        if t_b:
+            name = '.'.join([name, t_b])
+
+        l_r = ''
+        if 'L' in total and 'R' in total:
+            l_r = ''
+        elif 'L' in total:
+            l_r = 'L'
+        elif 'R' in total:
+            l_r = 'R'
+
+        if l_r:
+            name = '.'.join([name, l_r])
+
+        return name
+
+    def get_start_bones(self):
+        """
+        Returns all the bones starting a subchain of the face
+        :return:
+        """
+
+        bpy.ops.object.mode_set(mode='EDIT')
+        edit_bones = self.obj.data.edit_bones
+
+        start_bones = []
+
+        for name in self.bones['org'][1:]:
+            if not edit_bones[name].use_connect:
+                start_bones.append(name)
+
+        return start_bones
+
     def generate(self):
 
+        self.create_mch()
+        self.create_def()
         self.create_controls()
 
         return [""]
